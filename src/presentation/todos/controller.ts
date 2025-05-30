f926d5d135a33de4c49b4085fcfda0fc7176a4ea
franapostoli <2545886@ucc.edu.ar>
2025-05-30 11:47:39 -0300

    restserver con postgresql

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express';
+import { prisma } from '../../data/postgres';
+import { CreateTodoDto } from '../../domain/dtos/todos/create-todo.dto';
+import { UpdateTodoDto } from '../../domain/dtos';
 
 const todos = [
     { id: 1, title: 'Comprar leche', createdAt: new Date() },
@@ -12,70 +15,82 @@ export class TodosController {
     constructor() {
 
     }
+    // to convert the following methods with prisma is necesary add async and await. 
+    public getTodos = async (req: Request, res: Response) => {
 
-    public getTodos = (req: Request, res: Response) => {
+        const todos = await prisma.todo.findMany();
 
         return res.json(todos)
     }
 
-    public getTodoById = (req: Request, res: Response) => {
+    public getTodoById = async (req: Request, res: Response) => {
         const id = +req.params.id;
         if (isNaN(id)) {
             return res.status(400).json({ message: 'Id must be a number' });
         }
 
-        const todo = todos.find(t => t.id === id);
+        const todo = await prisma.todo.findFirst({
+            where: { id }
+        });
+        //const todo = todos.find(t => t.id === id);
 
         (todo)
             ? res.json(todo)
             : res.status(404).json({ message: 'Id not found' });
     }
 
-    public createTodo = (req: Request, res: Response) => {
-        const { text } = req.body;
-        if (!text) {
-            return res.status(400).json({ message: 'Text is required' });
-        }
-        todos.push(
-            { id: todos.length + 1, title: text, createdAt: new Date() }
-        )
-        res.json('POST create todo')
+    public createTodo = async (req: Request, res: Response) => {
+        const [error, createTodoDto] = CreateTodoDto.create(req.body);
+
+        if (error) return res.status(400).json({ message: error });
+        //const { text } = req.body;
+
+
+        const todo = await prisma.todo.create({
+            data: createTodoDto!
+        });
+
+        res.json(todo);
     }
 
-    public updateTodo = (req: Request, res: Response) => {
+    public updateTodo = async (req: Request, res: Response) => {
         const id = +req.params.id;
-        if (isNaN(id)) {
-            return res.status(400).json({ message: 'Id must be a number' });
-        }
+        const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
+        if (error) return res.status(400).json({ message: error });
 
-        const todo = todos.find(t => t.id === id);
+        const todo = await prisma.todo.findFirst({
+            where: { id }
+        })
+
 
         if (!todo) {
             return res.status(404).json({ message: 'Id not found' });
         }
 
-        const { text, createdAt } = req.body;
-        if (!text) {
-            return res.status(400).json({ message: 'Text is required' });
-        }
+        
+        const updatedTodo = await prisma.todo.update({
+            where: { id },
+            data: updateTodoDto!.values
+        });
 
-        todo.title = text || todo.title;
-        (createdAt === 'null')
-            ? todo.createdAt = null
-            : todo.createdAt = new Date(createdAt || todo.createdAt);
 
-        return res.json(todo);
+        return res.json(updatedTodo);
     }
 
 
-    public deleteTodo = (req: Request, res: Response) => {
-        const  id  = +req.params.id;
+    public deleteTodo = async (req: Request, res: Response) => {
+        const id = +req.params.id;
         const todo = todos.find(t => t.id === id);
         if (!todo) return res.status(404).json({ message: 'Id not found' });
-        
-        todos.splice(todos.indexOf(todo), 1);
 
-        res.json(todo)
+        const deletedTodo = await prisma.todo.delete({
+            where: { id }
+        });
+
+        (deletedTodo)
+            ? res.json(deletedTodo)
+            : res.status(404).json({ error: 'Id not found' });
+
     }
 
 }
